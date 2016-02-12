bfc7956fe709c53732caeb49eb208b56f09427ca
Alex Morton-Cooke <sgamorto@liv.ac.uk>
2016-02-12 12:32:55 +0000

    Outline for truth_table_mode

--- src/truth_table_mode.cpp
+++ src/truth_table_mode.cpp
@@ -1,6 +1,18 @@
 #include "truth_table_mode.hpp"
 
+#include "formula_string_parser_t.hpp"
+#include "formula_string_t.hpp"
+#include "formula_t.hpp"
+#include "truth_table_t.hpp"
+
 void truth_table_mode(int argc, char** argv) {
+
+	formula_string_parser_t parser;
+	formula_string_t formula_string = parser(string(argv[0]));
+	formula_t formula(formula_string);
+
+	truth_table_t truth_table(formula);
+	truth_table.print();
 
 }
 
